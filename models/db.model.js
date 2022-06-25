4aeb104e20c50ba8295426375bb006425a0598b4
popatre <76402245+popatre@users.noreply.github.com>
2022-06-25 14:07:32 +0100

    set up atlas config

--- models/db.model.js
+++ models/db.model.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 const findOrCreate = require("mongoose-findorcreate");
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+const password = process.env.PASSWORD;
+
+mongoose.connect(
+    `mongodb+srv://admin-jon:${password}@cluster0.cuozro0.mongodb.net/userDB`,
+    { useNewUrlParser: true }
+);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
